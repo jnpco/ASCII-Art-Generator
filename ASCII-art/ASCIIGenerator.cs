ff06e5232362d58424349bf1a2fd2a21e1712f06
JOR <jnpco.98@gmail.com>
2019-03-08 19:02:07 +1100

    Added rgb to hsp convertion.

--- ASCII-art/ASCIIGenerator.cs
+++ ASCII-art/ASCIIGenerator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.IO;
 
 namespace ASCII_art
 {
@@ -16,9 +17,29 @@ namespace ASCII_art
             this.imageLoc = imageLoc;
         }
 
-        public void convert(string saveLoc)
+        public void Convert(string saveLoc)
         {
-            var bmp = new Bitmap();
+            var bmp = new Bitmap(imageLoc);
+            using (var writer = new StreamWriter(saveLoc))
+            {
+                for (var y = 0; y < bmp.Height; y++)
+                {
+                    for (var x = 0; x < bmp.Width; x++)
+                    {
+                        var color = bmp.GetPixel(x, y);
+                    }
+                }
+            }
+        }
+
+        //http://alienryderflex.com/hsp.html
+        private double RGBToHSP(Color color)
+        {
+            return Math.Sqrt(
+                color.R * color.R * 0.299 +
+                color.G * color.G * 0.587 +
+                color.B * color.B * 0.114
+                );
         }
     }
 }
