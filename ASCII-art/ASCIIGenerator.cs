c470202463f0a9a081b2e9e47f1daad49d6a1dc6
JOR <jnpco.98@gmail.com>
2019-03-08 18:39:11 +1100

    Initial commit.

---
+++ ASCII-art/ASCIIGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ASCII_art
+{
+    class ASCIIGenerator
+    {
+        string imageLoc;
+
+        public ASCIIGenerator(string imageLoc)
+        {
+            this.imageLoc = imageLoc;
+        }
+
+        public void convert(string saveLoc)
+        {
+            var bmp = new Bitmap();
+        }
+    }
+}
