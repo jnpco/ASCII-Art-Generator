938fc44db882155707eaf4cc3f7fadcae5a71768
JOR <jnpco.98@gmail.com>
2019-03-10 15:47:37 +1100

    Added contrast utility method in ImageUtils.

--- ASCII-art/ImageUtils.cs
+++ ASCII-art/ImageUtils.cs
@@ -9,7 +9,7 @@ namespace ASCII_art
         /**
          * Resizes image keeping aspect ratio.
          */
-        public Bitmap ResizeImage(Bitmap bmp, int width)
+        public static Bitmap ResizeImage(Bitmap bmp, int width)
         {
             var ratio = (double)bmp.Height / bmp.Width;
             var height = (int)(width * ratio);
@@ -31,9 +31,44 @@ namespace ASCII_art
         }
 
         /**
+         * Sets Contrast of image.
+         * threshold should be a value between -100 and 100
+         */
+        public static Bitmap SetContrast(Bitmap bmp, int threshold)
+        {
+            var modifiedContrast = new Bitmap(bmp);
+            var lockedBitmap = new LockBitmap(modifiedContrast);
+            lockedBitmap.LockBits();
+
+            var contrast = Math.Pow((100.0 + threshold) / 100.0, 2);
+            for (int y = 0; y < lockedBitmap.Height; y++)
+            {
+                for (int x = 0; x < lockedBitmap.Width; x++)
+                {
+                    var oldColor = lockedBitmap.GetPixel(x, y);
+                    var red = ((((oldColor.R / 255.0) - 0.5) * contrast) + 0.5) * 255.0;
+                    var green = ((((oldColor.G / 255.0) - 0.5) * contrast) + 0.5) * 255.0;
+                    var blue = ((((oldColor.B / 255.0) - 0.5) * contrast) + 0.5) * 255.0;
+
+                    if (red > 255) red = 255;
+                    if (red < 0) red = 0;
+                    if (green > 255) green = 255;
+                    if (green < 0) green = 0;
+                    if (blue > 255) blue = 255;
+                    if (blue < 0) blue = 0;
+
+                    var newColor = Color.FromArgb(oldColor.A, (int)red, (int)green, (int)blue);
+                    lockedBitmap.SetPixel(x, y, newColor);
+                }
+            }
+            lockedBitmap.UnlockBits();
+            return modifiedContrast;
+        }
+
+        /**
          * Creates a new bitmap from the bitmap argument, which is helpful to avoid indexed bitmaps.
          */
-        public Bitmap Grayscale(Bitmap bmp)
+        public static Bitmap Grayscale(Bitmap bmp)
         {
             var grayScaled = new Bitmap(bmp);
             for (var y = 0; y < grayScaled.Height; y++)
@@ -52,7 +87,7 @@ namespace ASCII_art
          * http://alienryderflex.com/hsp.html
          * Converts rgb to grayscale using hsp color model.
          */
-        public double RGBToHSP(Color color)
+        public static double RGBToHSP(Color color)
         {
             return Math.Sqrt(
                 color.R * color.R * 0.299 +
